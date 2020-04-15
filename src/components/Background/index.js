034cf18a65aa70942e517dd0da57706fcede16cb
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-04-15 19:51:54 -0300

    feature: update profile

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,10 +1,8 @@
 import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
-const Background = styled(LinearGradient).attrs({
+export default styled(LinearGradient).attrs({
   colors: ['#7159c1', '#ab59c1'],
 })`
   flex: 1;
 `;
-
-export default Background;
